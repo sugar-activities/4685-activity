afeff99a39c02b2a94ca2a552e3d22f9e5c623da
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:26:00 -0400

    Initial commit

---
+++ lessons.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from gettext import gettext as _
+import glib
+
+LESSONS = (
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 11')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Rule #10: ')) + \
+        '</span>' + \
+        glib.markup_escape_text(_('We are all connected.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Pickup Points:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • We are all connected by the spark of Light within.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Both our reactive behaviors and our sharing behaviors affect others.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Our reactive behaviors disconnect us from the Light and from others.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • Our sharing behaviors connect us to the Light and to others.')) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Words to Know:')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('Human Dignity: Giving everyone the basic level of respect that you would expect for yourself')) + \
+        '\n' + \
+        glib.markup_escape_text(_("Tolerance: To accept other people’s differences and give them the freedom to be themselves")) + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Use your creative skills to draw a picture of a time you treated someone with human dignity. Write a short story about it.')) + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Use your creative skills to draw a picture of a time you treated someone with tolerance. Write a short story about it.')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 12 (The Hole in the Boat)')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Think of a time you had a reactive behavior? Explain what happened.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What were the results of your reactive behavior? Why?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What can you change about yourself to make this world a happier place?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Lesson 12')) + \
+        '</span>' + \
+        '\n\n' + \
+        '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Journal')) + \
+        '</span>' + \
+        '\n\n' + \
+        glib.markup_escape_text(_('   • Think of a time you had a reactive behavior? Explain what happened.')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What were the results of your reactive behavior? Why?')) + \
+        '\n' + \
+        glib.markup_escape_text(_('   • What can you change about yourself to make this world a happier place?')),
+
+    '<span size="x-large" weight="bold">' + \
+        glib.markup_escape_text(_('Epilogue')) + \
+        '</span>' + \
+        '\n\n',
+)
---
+++ videos.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+VIDEOS = (
+    ("11.ogv",
+     _("We are All Connected"),
+     _("We are all connected by the spark of Light within.\n\
+Both our reactive behaviors and our sharing behaviors affect others.\n\
+Our reactive behaviors disconnect us from the Light and from others.\n\
+Our sharing behaviors connect us to the Light and to others."),
+      ),
+    ("12.1.ogv",
+     _("The Hole in the Boat"),
+     '',
+     ),
+    ("12.4.ogv",
+     _("Change Begins with Me"),
+     '',
+     ),
+    ("13.ogv",
+     _('Epilogue'),
+     '',
+     ),
+)
